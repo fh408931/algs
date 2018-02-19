420766bd9e940aa9ea6778dacac7b4ecd91b9e4b
fh408931 <fh408931@163.com>
2018-02-20 01:26:24 +0800

    2

---
+++ src/main/java/algorithms/fundamentals/basic/Matrix.java
@@ -0,0 +1,22 @@
+package algorithms.fundamentals.basic;
+
+public class Matrix {
+	/*
+	 * 
+	 */
+	public static double dot(double[] x,double[] y){
+		return 1.0;
+	}
+	public static double[][] mult(double[][] a,double[][]b){
+	
+	}
+	public static double[][] transpose(double[][] a){
+	
+	}
+	public static double[] mult(double[][] a,double[] x){
+	
+	}
+	public static double[] mult(double[] y, double[][] a){
+	
+	}
+}
